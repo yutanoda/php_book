0082b23b7cfa50a2c993f38178022874e0bb80d5
yutanoda <atuyuta902@yahoo.co.jp>
2020-09-27 08:07:16 +0000

    5.3.2

---
+++ chapter5/index.php
@@ -0,0 +1,76 @@
+<?php
+class Employee
+{
+    private $name;
+    private $type;
+    public static $company = "技評技術社";
+    private $state = "働いている";
+    public $salary = 20;
+    
+    const PARTTIME = 0x01;
+    const REGULAR = 0x02;
+    const CONTRACT = 0x03;
+    
+    public function __construct($name, $type)
+    {
+        $this->name = $name;
+        $this->type = $type;
+    }
+    
+    function getState()
+    {
+        return $this->state;
+    }
+    function setState($state)
+    {
+        $this->state = $state;
+    }
+    
+    
+    public function work() 
+    {
+        echo '書類を整理しています。', PHP_EOL;
+    }
+    
+    public static function getCompany() {
+        return self::$company;
+    }
+    
+    public static function setCompany($value)
+    {
+        self::$company = $value;
+    }
+    
+}
+
+class Programmer extends Employee
+{
+    public function __construct($name, $type)
+    {
+        parent::__construct($name, $type);
+    }
+}
+
+interface Reader
+{
+    public function read();
+}
+
+class Configure implements Reader
+{
+    
+}
+
+//
+
+class Employee
+{
+    public function __toString()
+    {
+        return 'This class is: ' . __CLASS__;
+    }
+}
+$yamada = new Employee();
+echo $yamada;
+
+//
--- index.php
+++ index.php
@@ -1,62 +1,25 @@
 <?php
-class Employee
-{
-    private $name;
-    private $type;
-    public static $company = "技評技術社";
-    private $state = "働いている";
-    public $salary = 20;
-    
-    const PARTTIME = 0x01;
-    const REGULAR = 0x02;
-    const CONTRACT = 0x03;
-    
-    public function __construct($name, $type)
-    {
-        $this->name = $name;
-        $this->type = $type;
-    }
-    
-    function getState()
-    {
-        return $this->state;
-    }
-    function setState($state)
-    {
-        $this->state = $state;
-    }
-    
-    
-    public function work() 
-    {
-        echo '書類を整理しています。', PHP_EOL;
-    }
-    
-    public static function getCompany() {
-        return self::$company;
-    }
-    
-    public static function setCompany($value)
-    {
-        self::$company = $value;
-    }
-    
-}
 
-class Programmer extends Employee
+class Foo
 {
-    public function __construct($name, $type)
+    public function helloGateway()
     {
-        parent::__construct($name, $type);
+        self::hello();
+    }
+    
+    public static function hello()
+    {
+        echo __CLASS__ . 'hello' . PHP_EOL;
     }
 }
 
-interface Reader
-{
-    public function read();
+class Bar extends Foo {
+    public static function hello()
+    {
+        echo __CLASS__ . 'hello' . PHP_EOL;
+    }
 }
 
-class Configure implements Reader
-{
-    
-}
+$bar = new Bar();
+echo $bar->helloGateway();
+
