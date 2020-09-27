002f30361940c222f95ebfab5aefbdbd48b42eb5
yutanoda <atuyuta902@yahoo.co.jp>
2020-09-27 08:53:41 +0000

    5.4.2

--- chapter5/lib.php
+++ chapter5/lib.php
@@ -73,4 +73,80 @@ class Employee
 $yamada = new Employee();
 echo $yamada;
 
-//
+//5.3.2
+
+class Foo
+{
+    public function helloGateway()
+    {
+        self::hello();
+    }
+    
+    public static function hello()
+    {
+        echo __CLASS__ . 'hello' . PHP_EOL;
+    }
+}
+
+class Bar extends Foo {
+    public static function hello()
+    {
+        echo __CLASS__ . 'hello' . PHP_EOL;
+    }
+}
+
+$bar = new Bar();
+echo $bar->helloGateway();
+
+//5.3.3
+function __autoload($name) {
+    $filename = $name . '.php';
+    if (is_readable($filename)) {
+        require $filename;
+    }
+}
+
+$obj = new Foo(); // 自動的にマジックメソッド__autoloadが読まれる
+
+//5.4.1
+
+//program.php
+
+require_once 'Cake.php';
+
+class Cake
+{
+    
+}
+
+//cake.php
+
+namespace Food\Sweets;
+
+$c = new Food\Sweets\Cake();
+
+//5.4.2
+
+
+//Project/Module/Directory.php
+
+namespace Project\Module;
+
+$dir = new Directory();
+$dir = new \Project\Module\Directory();
+
+// 先頭に名前空間を宣言している場合は　\がいる
+
+namespace Other;
+
+require_once 'Project/Module/Directory.php';
+
+$dir1 = new \Project\Module\Directory();
+
+$dir2 = new \Directory();
+
+//名前空間が宣言されていない場合
+
+require_once 'Project/Module/Directory.php';
+
+$dir = new Project\Module\Directory;
